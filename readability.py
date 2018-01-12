9c0fe320d064ce9a2a25404e85088f9e037bb26d
Kasey Ann <kaseystowell@Kaseys-MacBook-Pro.local>
2018-01-11 21:41:25 -0500

    added forumula

--- readability.py
+++ readability.py
@@ -3,6 +3,8 @@ from readability.Inspector import Inspector
 if __name__ == "__main__":
     inspect = Inspector()
     #  Mac users: /Users/user_name/Documents/file
-    inspect.read(input("File path? \n"))
-    inspect.eval()
-
+    inspect.eval(inspect.read(input("File path? \n")))
+    stats = inspect.stats()
+    print("Total words: " + str(stats[0]) + ". Total sentences: " + str(stats[1]) + ". Total syllables: " +
+          str(stats[2]) + ".")
+    print(inspect.school_level(inspect.reading_ease()))
